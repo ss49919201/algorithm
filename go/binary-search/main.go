a0c9138fa2038d178d6c85ef4594ba10181dfc77
ss49919201 <sakashin10291029@gmail.com>
2023-05-09 23:25:36 +0900

    binary search in Go

---
+++ go/binary-search/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+var arr []int
+var tgt int
+
+func main() {
+	arr = []int{1, 3, 5, 7, 10, 13, 17}
+	tgt = 9
+	fmt.Println(run(0, len(arr)-1))
+
+	tgt = 7
+	fmt.Println(run(0, len(arr)-1))
+
+	tgt = 3
+	fmt.Println(run(0, len(arr)-1))
+}
+
+func run(low, high int) bool {
+	if low > high {
+		return false
+	}
+
+	mid := (high + low) / 2
+	midv := arr[mid]
+
+	fmt.Println(midv)
+
+	switch {
+	case midv == tgt:
+		return true
+	case midv < tgt:
+		return run(mid+1, high)
+	default:
+		return run(low, mid-1)
+	}
+}
