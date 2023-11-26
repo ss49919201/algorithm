99661d506d0b222120d8f6593d9164d0f6d1f3c5
ss49919201 <sakashin10291029@gmail.com>
2023-11-26 18:24:17 +0900

    refactor: reuse array

--- javascript/search/binary-search/main.js
+++ javascript/search/binary-search/main.js
@@ -1,26 +1,18 @@
-function search(arr, tgt) {
-    if (arr.length === 0) {
-        return false;
-    }
+function search(arr, tgt, start, end) {
+    if (start > end) return false;
  
-    if (arr.length === 1) {
-        return arr[0] === tgt;
-    }
-
-    const mid = Math.floor(arr.length / 2)
-    if (arr[mid] === tgt) {
-        return true;
-    }
+    const mid = Math.floor((start+end) / 2)
+    if (arr[mid] === tgt) return true;
 
     if (arr[mid] > tgt) {
-        return search(arr.slice(0, mid), tgt);
+        return search(arr, tgt, start, mid-1);
     } else {
-        return search(arr.slice(mid), tgt);
+        return search(arr, tgt, mid+1, end);
     }
 };
 
-console.log(search([1, 3, 100, 1_000], 1_000));
-console.log(search([1, 3, 100, 1_000], 1));
-console.log(search([1, 3], 2));
-console.log(search([1], 2));
-console.log(search([], 2));
+console.log(search([1, 3, 100, 1_000], 1_000, 0, 3));
+console.log(search([1, 3, 100, 1_000], 1, 0, 3));
+console.log(search([1, 3], 2, 0, 1));
+console.log(search([1], 2, 0, 0));
+console.log(search([], 2, 0, 0));
