3296b884151dd9e4f7f87ecb0950b3d03da7b0d6
ss49919201 <sakashin10291029@gmail.com>
2023-11-26 18:03:37 +0900

    feat: binary search

---
+++ javascript/search/binary-search/main.js
@@ -0,0 +1,25 @@
+function search(arr, tgt) {
+    if (arr.length === 0) {
+        return false;
+    }
+ 
+    if (arr.length === 1) {
+        return arr[0] === tgt;
+    }
+
+    const mid = Math.round(arr.length / 2)
+    if (arr[mid] === tgt) {
+        return true;
+    }
+
+    if (arr[mid] > tgt) {
+        return search(arr.slice(0, mid-1), tgt);
+    } else {
+        return search(arr.slice(mid), tgt);
+    }
+};
+
+console.log(search([1, 3, 100, 1_000], 1_000));
+console.log(search([1, 3], 2));
+console.log(search([1], 2));
+console.log(search([], 2));
